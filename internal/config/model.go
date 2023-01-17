ba64b77ecc531e261b54f457ca0d41461028a083
stnokott <nftrash@web.de>
2023-01-17 02:15:18 +0100

    feat: add InfluxDB org to config

--- internal/config/model.go
+++ internal/config/model.go
@@ -11,6 +11,7 @@ const (
 	CONFIG_KEY_MATCH_REPLAY_FOLDER string = "game.replay_folder"
 	CONFIG_KEY_INFLUX_DB_HOST      string = "influx.host"
 	CONFIG_KEY_INFLUX_DB_PORT      string = "influx.port"
+	CONFIG_KEY_INFLUX_DB_ORG       string = "influx.org"
 	CONFIG_KEY_INFLUX_DB_BUCKET    string = "influx.bucket"
 	CONFIG_KEY_INFLUX_DB_TOKEN     string = "influx.token"
 
@@ -21,6 +22,7 @@ type Config struct {
 	MatchReplyFolder string
 	InfluxDBHost     string
 	InfluxDBPort     int
+	InfluxDBOrg      string
 	InfluxDBBucket   string
 	InfluxDBToken    string
 }
@@ -32,6 +34,7 @@ var (
 	bindInfluxHost     = binding.BindString(&Current.InfluxDBHost)
 	bindInfluxPort     = binding.BindInt(&Current.InfluxDBPort)
 	bindInfluxPortStr  = binding.IntToString(bindInfluxPort)
+	bindInfluxOrg      = binding.BindString(&Current.InfluxDBOrg)
 	bindInfluxBucket   = binding.BindString(&Current.InfluxDBBucket)
 	bindInfluxToken    = binding.BindString(&Current.InfluxDBToken)
 )
@@ -41,6 +44,7 @@ func Init(app fyne.App) {
 	Current.MatchReplyFolder = prefs.StringWithFallback(CONFIG_KEY_MATCH_REPLAY_FOLDER, "")
 	Current.InfluxDBHost = prefs.StringWithFallback(CONFIG_KEY_INFLUX_DB_HOST, "")
 	Current.InfluxDBPort = prefs.IntWithFallback(CONFIG_KEY_INFLUX_DB_PORT, CONFIG_DEFAULT_INFLUX_DB_PORT)
+	Current.InfluxDBOrg = prefs.StringWithFallback(CONFIG_KEY_INFLUX_DB_ORG, "")
 	Current.InfluxDBBucket = prefs.StringWithFallback(CONFIG_KEY_INFLUX_DB_BUCKET, "")
 	Current.InfluxDBToken = prefs.StringWithFallback(CONFIG_KEY_INFLUX_DB_TOKEN, "")
 }
@@ -49,6 +53,7 @@ func Write() {
 	prefs.SetString(CONFIG_KEY_MATCH_REPLAY_FOLDER, Current.MatchReplyFolder)
 	prefs.SetString(CONFIG_KEY_INFLUX_DB_HOST, Current.InfluxDBHost)
 	prefs.SetInt(CONFIG_KEY_INFLUX_DB_PORT, Current.InfluxDBPort)
+	prefs.SetString(CONFIG_KEY_INFLUX_DB_ORG, Current.InfluxDBOrg)
 	prefs.SetString(CONFIG_KEY_INFLUX_DB_BUCKET, Current.InfluxDBBucket)
 	prefs.SetString(CONFIG_KEY_INFLUX_DB_TOKEN, Current.InfluxDBToken)
 }
--- internal/config/view.go
+++ internal/config/view.go
@@ -8,6 +8,7 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/validation"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
@@ -19,25 +20,36 @@ const (
 	hintMatchReplayDir string = "Directory where your match replays are stored. Usually in your game folder named 'MatchReplays'"
 	hintInfluxHost     string = `Host of your InfluxDB instance (IP or hostname, no "http(s)" required)`
 	hintInfluxPort     string = "Port of your InfluxDB instance"
+	hintInfluxOrg      string = "InfluxDB organization to connect to"
 	hintInfluxBucket   string = "InfluxDB bucket where the data should be pushed"
 	hintInfluxToken    string = "InfluxDB token for the bucket"
 )
 
 var (
-	inputMatchReplayDir = &widget.Entry{Validator: validateMatchReplayDir}
-	inputInfluxHost     = &widget.Entry{Validator: validateInfluxHost}
-	inputInfluxPort     = &widget.Entry{Validator: validateInfluxPort}
-	inputInfluxBucket   = &widget.Entry{Validator: validateNotEmpty}
-	inputInfluxToken    = &widget.Entry{Validator: validateNotEmpty, Password: true}
+	inputMatchReplayDir = &widget.Entry{}
+	inputInfluxHost     = &widget.Entry{}
+	inputInfluxPort     = &widget.Entry{}
+	inputInfluxOrg      = &widget.Entry{}
+	inputInfluxBucket   = &widget.Entry{}
+	inputInfluxToken    = &widget.Entry{Password: true}
 )
 
 func ShowDialog(parent fyne.Window) {
-	// unfortunately cant initialize with data binding *and* validator, so need to set here
+	// 1. can't use NewEntryWithData on global level
+	// 2. calling .Bind() removes validators, so we need to set them here (instead of when constructing the entry, see https://github.com/fyne-io/fyne/issues/2542)
 	inputMatchReplayDir.Bind(bindMatchReplayDir)
 	inputInfluxHost.Bind(bindInfluxHost)
 	inputInfluxPort.Bind(bindInfluxPortStr)
+	inputInfluxOrg.Bind(bindInfluxOrg)
 	inputInfluxBucket.Bind(bindInfluxBucket)
 	inputInfluxToken.Bind(bindInfluxToken)
+
+	inputMatchReplayDir.Validator = validation.NewAllStrings(requiredValidator, directoryValidator)
+	inputInfluxHost.Validator = validation.NewAllStrings(requiredValidator, hostAddressValidator)
+	inputInfluxPort.Validator = validation.NewAllStrings(requiredValidator, integerValidator)
+	inputInfluxOrg.Validator = requiredValidator
+	inputInfluxBucket.Validator = requiredValidator
+	inputInfluxToken.Validator = requiredValidator
 
 	buttonAutodetectMatchReplayDir := widget.NewButton("Autodetect", func() {
 		folder, err := matchReplayFolderFromRegistry()
@@ -62,6 +74,7 @@ func ShowDialog(parent fyne.Window) {
 		{Text: "", Widget: container.NewGridWithColumns(2, buttonAutodetectMatchReplayDir, buttonSelectMatchReplayDir)},
 		{Text: "InfluxDB host", Widget: inputInfluxHost, HintText: hintInfluxHost},
 		{Text: "InfluxDB port", Widget: inputInfluxPort, HintText: hintInfluxPort},
+		{Text: "InfluxDB org", Widget: inputInfluxOrg, HintText: hintInfluxOrg},
 		{Text: "InfluxDB bucket", Widget: inputInfluxBucket, HintText: hintInfluxBucket},
 		{Text: "InfluxDB token", Widget: inputInfluxToken, HintText: hintInfluxToken},
 	}
@@ -83,20 +96,16 @@ func ShowDialog(parent fyne.Window) {
 }
 
 // TODO: warn if no matches found or none could be parsed
-func validateMatchReplayDir(s string) (err error) {
-	if s == "" {
-		err = errors.New("Cannot be empty") //lint:ignore ST1005 will be displayed in UI
-	} else {
-		stats, statErr := os.Stat(s)
-		if statErr != nil {
-			if os.IsNotExist(statErr) {
-				err = errors.New("Does not exist") //lint:ignore ST1005 will be displayed in UI
-			} else {
-				err = statErr
-			}
-		} else if !stats.IsDir() {
-			err = errors.New("Not a directory") //lint:ignore ST1005 will be displayed in UI
+func directoryValidator(s string) (err error) {
+	stats, statErr := os.Stat(s)
+	if statErr != nil {
+		if os.IsNotExist(statErr) {
+			err = errors.New("Does not exist") //lint:ignore ST1005 will be displayed in UI
+		} else {
+			err = statErr
 		}
+	} else if !stats.IsDir() {
+		err = errors.New("Not a directory") //lint:ignore ST1005 will be displayed in UI
 	}
 	return
 }
@@ -106,10 +115,8 @@ var (
 	regexHostname = regexp.MustCompile(`^(?:[0-9a-zA-Z]+\.)+[0-9a-zA-Z]{2,4}$`)
 )
 
-func validateInfluxHost(s string) (err error) {
-	if s == "" {
-		err = errors.New("Cannot be empty") //lint:ignore ST1005 will be displayed in UI
-	} else if regexIPv4.MatchString(s) {
+func hostAddressValidator(s string) (err error) {
+	if regexIPv4.MatchString(s) {
 		return
 	} else if regexHostname.MatchString(s) {
 		return
@@ -119,22 +126,18 @@ func validateInfluxHost(s string) (err error) {
 	return
 }
 
-func validateInfluxPort(s string) (err error) {
-	if s == "" {
-		err = errors.New("Cannot be empty") //lint:ignore ST1005 will be displayed in UI
-	} else {
-		var port int
-		port, err = strconv.Atoi(s)
-		if err != nil {
-			err = errors.New("Not a valid integer") //lint:ignore ST1005 will be displayed in UI
-		} else if port <= 0 {
-			err = errors.New("Must be greater than zero") //lint:ignore ST1005 will be displayed in UI
-		}
+func integerValidator(s string) (err error) {
+	var port int
+	port, err = strconv.Atoi(s)
+	if err != nil {
+		err = errors.New("Not a valid integer") //lint:ignore ST1005 will be displayed in UI
+	} else if port <= 0 {
+		err = errors.New("Must be greater than zero") //lint:ignore ST1005 will be displayed in UI
 	}
 	return
 }
 
-func validateNotEmpty(s string) (err error) {
+func requiredValidator(s string) (err error) {
 	if s == "" {
 		err = errors.New("Cannot be empty") //lint:ignore ST1005 will be displayed in UI
 	}

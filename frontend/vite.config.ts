7b02b43aa4136c16a37b6cd1599dfcbe957cfe91
Noah Kottenhahn <30934296+stnokott@users.noreply.github.com>
2023-03-17 11:45:05 +0100

    build: enable hot reload only in dev

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,8 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    disabled: process.env.NODE_ENV === "test"
-  },
-  plugins: [svelte()],
+  plugins: [svelte({
+    hot: process.env.NODE_ENV === "dev"
+  })],
   publicDir: "public"
 })

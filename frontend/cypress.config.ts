102bb4802ee8e1e632a4b3c71473000d5c0f6804
Noah Kottenhahn <30934296+stnokott@users.noreply.github.com>
2023-03-17 11:57:00 +0100

    ci: use @cypress/watch-preprocessor

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "cypress";
+import watch from "@cypress/watch-preprocessor";
 
 export default defineConfig({
   component: {
     devServer: {
       framework: "svelte",
       bundler: "vite",
+    },
+    setupNodeEvents(on, _config) {
+      on("file:preprocessor", watch())
     },
   },
   video: false
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  optimizeDeps: {
-    disabled: process.env.NODE_ENV === "test"
-  },
   plugins: [svelte()],
   publicDir: "public"
 })

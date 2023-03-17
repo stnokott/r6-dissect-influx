977b007dfe941466b85e06fe93d7cadcaab419d3
Noah Kottenhahn <30934296+stnokott@users.noreply.github.com>
2023-03-17 11:37:37 +0100

    ci: disable optimizeDeps when testing

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    disabled: process.env.NODE_ENV === "test"
+  },
   plugins: [svelte()],
   publicDir: "public"
 })

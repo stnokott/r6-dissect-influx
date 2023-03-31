c76297b0ecc0e1f6208ba6ddb11ab35cab84d8d9
Noah Kottenhahn <30934296+stnokott@users.noreply.github.com>
2023-03-31 11:26:36 +0200

    ci(Cypress): test with vite-plugin-warmup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { warmup } from 'vite-plugin-warmup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    warmup({
+      clientFiles: ['./cypress/support/component.ts', './src/**/*.cy.{js,jsx,ts,tsx}']
+    })
+  ],
   publicDir: "public"
 })
